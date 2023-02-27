73b36ecf82560d4d9e73473b95f5e93e7075c769
Yusuf <planetapluton888@gmail.com>
2023-02-27 07:48:59 +0600

    Replace upload file dialog design

--- app/src/main/java/com/example/bookloverfinalapp/app/ui/admin_screens/screen_upload_book/UploadFileDialog.kt
+++ app/src/main/java/com/example/bookloverfinalapp/app/ui/admin_screens/screen_upload_book/UploadFileDialog.kt
@@ -1,24 +1,24 @@
 package com.example.bookloverfinalapp.app.ui.admin_screens.screen_upload_book
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
+import com.example.bookloverfinalapp.R
 import com.example.bookloverfinalapp.app.utils.bindingLifecycleError
-import com.example.bookloverfinalapp.app.utils.extensions.runWithArgumentsOrSkip
-import com.example.bookloverfinalapp.app.utils.extensions.tuneLyricsDialog
+import com.example.bookloverfinalapp.app.utils.extensions.*
 import com.example.bookloverfinalapp.databinding.DialogUploadBookBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class UploadBookDialog : DialogFragment() {
+class UploadFileDialog : DialogFragment() {
 
     private var _binding: DialogUploadBookBinding? = null
     private val binding get() = _binding ?: bindingLifecycleError()
-
-    private val viewModel: FragmentUploadFileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -30,11 +30,20 @@ class UploadBookDialog : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         tuneLyricsDialog()
+        tuneCenterDialog()
         initAndApplyArguments()
     }
 
-    fun setTitle(progress: Int, title: String = "Загружаем книгу") {
-        binding.title.text = "$title $progress%"
+    fun setTitle(progress: Int, description: String = getString(R.string.loading_the_book)) {
+        val progressText = "$progress%"
+        binding.procent.text = progressText
+        binding.description.text = description
+    }
+
+    fun showSuccessBlock(action: () -> Unit) = with(binding) {
+        successBlock.isVisible = true
+        uploadBlock.isVisible = false
+        continueBtn.setOnDownEffectClickListener { action() }
     }
 
     private fun initAndApplyArguments() = runWithArgumentsOrSkip { arguments ->
@@ -49,8 +58,8 @@ class UploadBookDialog : DialogFragment() {
         private const val TITLE_KEY = "TITLE_KEY"
         private const val IS_CANCELABLE_KEY = "IS_CANCELABLE_KEY"
 
-        fun getInstance(title: String = String(), isCancelable: Boolean = false): UploadBookDialog {
-            val dialog = UploadBookDialog()
+        fun getInstance(title: String = String(), isCancelable: Boolean = false): UploadFileDialog {
+            val dialog = UploadFileDialog()
             val bundle = Bundle()
             bundle.putString(TITLE_KEY, title)
             bundle.putBoolean(IS_CANCELABLE_KEY, isCancelable)
